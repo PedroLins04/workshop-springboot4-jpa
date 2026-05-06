fe97d0086dc523c323a1e7b8f1c238bd77e1b8e1
PedroLins <phlins.olinda@gmail.com>
2026-05-05 23:33:23 -0400

    Order entitie

---
+++ src/main/java/com/web/backweb/entities/Order.java
@@ -0,0 +1,85 @@
+package com.web.backweb.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    //CONSTRUCTORS
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        Id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    //GETS AND SETS
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    //HASH AND EQUALS
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(Id, order.Id) && Objects.equals(moment, order.moment) && Objects.equals(client, order.client);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, moment, client);
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "Id=" + Id +
+                ", moment=" + moment +
+                ", client=" + client +
+                '}';
+    }
+}
