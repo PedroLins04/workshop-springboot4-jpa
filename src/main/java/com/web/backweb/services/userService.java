c371669fd18384b39a138edba8a5f43e937a3795
PedroLins <phlins.olinda@gmail.com>
2026-05-05 20:22:35 -0400

    service layer, component registration

---
+++ src/main/java/com/web/backweb/services/userService.java
@@ -0,0 +1,26 @@
+package com.web.backweb.services;
+
+import com.web.backweb.entities.User;
+import com.web.backweb.repositories.userRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class userService {
+
+    @Autowired
+    private userRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long Id) {
+       Optional<User> obj = repository.findById(Id);
+       return obj.get();
+    }
+
+}
